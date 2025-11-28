2a39199dbe2696486f735f5b102cad19d1aa5418
Khushi Tala <khushi02alphabitinfoway@gmail.com>
2025-11-28 17:44:55 +0530

    feat ;- about page ui

--- assets/js/main.js
+++ assets/js/main.js
@@ -201,3 +201,20 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
 });
+
+
+// our history
+
+ const items = document.querySelectorAll('.timeline-item');
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      }
+    });
+  }, {
+    threshold: 0.3
+  });
+
+  items.forEach(item => observer.observe(item));
